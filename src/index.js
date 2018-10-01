aee9effbc80e454e25d2752b63f3b1da916fab13
Rutkevich Sasha <rutkevich.sawa@gmail.com>
2018-10-01 19:37:18 +0300

    working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+   var int = 2;
+   var index = 0;
+   var arr = [];
+   var count = 0;
+   while (base > 1)
+   {
+       while (base % int == 0)
+       {
+           base = base / int;
+           arr.push(int);
+       }
+       int++;
+   }
+
+   for(var i = 0; i < arr.length; ++i){
+       if(arr[i] == arr[arr.length - 1])
+           count++;
+   }
+
+
+   while(number > 0)
+   {
+       if(number>1){
+           number /= int-1;
+           index += Math.floor(number);
+       }else{
+           break;
+       }
+   }
+   return Math.floor(index/count);
 }
